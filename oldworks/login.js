4a7dc40b7983ce8069025fd11e9c72806740e9f7
growhigh <growhigh3@gmail.com>
2022-07-23 16:33:12 +0100

    my first commit

---
+++ oldworks/login.js
@@ -0,0 +1,19 @@
+const name = document.querySelector('#name')
+const username = document.querySelector('#username')
+const password = document.querySelector('#password')
+const phonenumber = document.querySelector('#phonenumber')
+const formatic= document.querySelector('#formatic')
+const register= document.querySelector('#register')
+
+form.addEventListener('click',alert('Namaste'))
+
+
+form .onclick(function () {
+    let messages =[]
+    if (username.value ==="" || username.value == null){
+        alert('welcome')
+    }
+
+})
+
+
